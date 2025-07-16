853fc4cf570144abcb1f02ae75a9e169bd2d1dcd
PhatMenghor <phatmenghor19@gmail.com>
2025-07-16 13:43:16 +0700

    add

--- src/main/java/com/menghor/ksit/constants/SuccessMessages.java
+++ src/main/java/com/menghor/ksit/constants/SuccessMessages.java
@@ -1,6 +1,5 @@
 package com.menghor.ksit.constants;
 
 public class SuccessMessages {
-    public static final String SUCCESS = "Success";
     public static final String PASSWORD_CHANGED_SUCCESSFULLY = "Password changed successfully.";
 }
