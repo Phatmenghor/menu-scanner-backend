cfc76137593ad5d1699d66d10eb4c04a3fbd4367
Phatmenghor <phatmenghor19@gmail.com>
2025-07-19 15:59:09 +0700

    Auto commit on 2025-07-19 15:59:09

--- src/main/java/com/emenu/config/SwaggerConfig.java
+++ src/main/java/com/emenu/config/SwaggerConfig.java
@@ -1,18 +1,19 @@
 package com.emenu.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -41,7 +42,7 @@ public class SwaggerConfig {
                                 .name("Proprietary")
                                 .url("https://emenu-platform.com/license")))
                 .servers(List.of(
-                        new Server().url("http://localhost:9090").description("Development server"),
+                        new Server().url("http://localhost:8080").description("Development server"),
                         new Server().url("https://api.emenu-platform.com").description("Production server")))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
