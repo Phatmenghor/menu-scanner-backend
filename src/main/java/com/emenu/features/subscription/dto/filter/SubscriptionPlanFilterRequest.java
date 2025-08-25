2a176e75f9f38950f03ed5b274203bc0c2b96165
Phat Menghor <phatmenghor19@gmail.com>
2025-08-26 00:20:54 +0700

    Auto commit on 2025-08-26 00:20:54

--- src/main/java/com/emenu/features/subscription/dto/filter/SubscriptionPlanFilterRequest.java
+++ src/main/java/com/emenu/features/subscription/dto/filter/SubscriptionPlanFilterRequest.java
@@ -2,8 +2,6 @@ package com.emenu.features.subscription.dto.filter;
 
 import com.emenu.enums.sub_scription.SubscriptionPlanStatus;
 import com.emenu.shared.dto.BaseFilterRequest;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -13,12 +11,10 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class SubscriptionPlanFilterRequest extends BaseFilterRequest {
-    private SubscriptionPlanStatus status;
     private List<SubscriptionPlanStatus> statuses;
     private BigDecimal minPrice;
     private BigDecimal maxPrice;
     private Integer minDurationDays;
     private Integer maxDurationDays;
-    private Boolean publicOnly = false;
     private Boolean freeOnly = false;
 }
