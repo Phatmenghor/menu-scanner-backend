5064596053d5871c2a623827a3d5d09495308137
Phat Menghor <menghor.phat@cambodiapostbank.com.kh>
2025-12-04 11:22:45 +0700

    [development] Auto commit on 2025-12-04 11:22:45 | Files: src/main/java/com/emenu/features/auth/controller/ATokenContorller.java

--- src/main/java/com/emenu/features/auth/controller/ZTokenContorller.java
+++ src/main/java/com/emenu/features/auth/controller/ZTokenContorller.java
@@ -1,9 +1,6 @@
 package com.emenu.features.auth.controller;
 
 import com.emenu.features.auth.dto.filter.BusinessFilterRequest;
-import com.emenu.features.auth.dto.response.BusinessResponse;
-import com.emenu.shared.dto.ApiResponse;
-import com.emenu.shared.dto.PaginationResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,16 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/atoken")
+@RequestMapping("/api/v1/token")
 @RequiredArgsConstructor
 @Slf4j
-public class ATokenContorller {
+public class ZTokenContorller {
 
     @PostMapping("")
-    public ResponseEntity<String> getAllBusinesses(
-            @Valid @RequestBody BusinessFilterRequest request) {
-        log.info("Get all businesses");
-
+    public ResponseEntity<String> getMyToken() {
+        log.info("Get my token");
         return ResponseEntity.ok("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaGF0bWVuZ2hvcjE5QGdtYWlsLmNvbSIsInJvbGVzIjoiUk9MRV9QTEFURk9STV9PV05FUiIsImlhdCI6MTc2NDgyMjAxOCwiZXhwIjoxMDAwMDE3NjQ4MjIwMTh9.8cFubNvLItiFIjpthDDnmuc7V0A_rL_UdiUsJbeZjPEGB2HUT5lYTMJHiZrfhXXj3IIXa562tUzNjpAwvKmxYw");
     }
 }
